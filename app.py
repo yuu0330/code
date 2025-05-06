4805f58e1c082f54dff9c9f2a90c10ea9898d870
qoiaaabb <qb930809@gmail.com>
2025-05-06 14:47:57 +0800

    0506

--- app.py
+++ app.py
@@ -678,7 +678,7 @@ def gen_frames():
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
             
 @app.route("/one")
 def video():
